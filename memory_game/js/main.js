1ebf6960139dbd6e86c9940fe339c0a4b35cc187
Caio Ingber <caioingber@gmail.com>
2019-12-30 18:43:31 -0500

    updated main.js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,24 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cards = ["queen", "king", "king", "king"];
+const cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+var cardThree = cards[2];
+var cardFour = cards[3];
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+};
 
 console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
 console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
