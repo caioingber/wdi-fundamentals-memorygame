64fceb1083196207d08b10e57fb4c4d16e346372
Caio Ingber <caioingber@gmail.com>
2020-01-03 18:52:51 -0500

    complete w/ reset button

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,24 +21,46 @@ const cards = [
 }
 ];
 
-const cardsInPlay = [];
+var cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-			console.log("You found a match!");
+			alert("You found a match!");
 		}
 		else {
-			console.log("Sorry, try again.");
+			alert("Sorry, try again.");
 		}
 	}
-};
+}
 
-function flipCard(cardId) {
-	cardsInPlay.push(cards[cardId].rank);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);	
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 };
 
+function reset() {
+	for (var i = 0; i < cards.length; i++) {
+		document.getElementsByTagName('img')[i].setAttribute('src', 'images/back.png');
+	}
+	cardsInPlay = [];
+}
+
+document.getElementsByTagName('button')[0].addEventListener('click', reset);
+
+createBoard();
